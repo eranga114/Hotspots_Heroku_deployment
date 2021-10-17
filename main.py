fea02027e8f9de2f9028746799cbe18ac1b5cf0e
eranga114 <62519225+eranga114@users.noreply.github.com>
2021-10-17 09:50:08 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import pickle
+
+with open( 'rfc_model_pickle', 'rb' ) as file:
+    rf_pickle = pickle.load( file )
+
+app = FastAPI()
+
+
+class Item( BaseModel ):
+    startPosition: int
+    mut_count: int
+    total_mut_count: int
+
+
+@app.get( '/' )
+def home():
+    return {'message': 'Welcome to Hotspots analyzer'}
+
+
+@app.post( '/hotspots' )
+def hotspots_pred(data: Item):
+    data = data.dict()
+    startPosition = data['startPosition']
+    mut_count = data['mut_count']
+    total_mut_count = data['total_mut_count']
+
+    prediction = rf_pickle.predict( [[startPosition, mut_count, total_mut_count]] )
+    if prediction == 1:
+        hotspot = 'Its a cancer hotspot'
+    else:
+        hotspot = 'Its not a cancer hotspot'
+
+    return {
+        "Cancer Hotspot Prediction": hotspot
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run( app, host='localhost', port=8888 )
