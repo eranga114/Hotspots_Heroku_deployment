da16c7bc56d8450cb8d992b24f4439cef958f42e
eranga114 <62519225+eranga114@users.noreply.github.com>
2021-10-18 13:11:38 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import uvicorn
-from fastapi import FastAPI, Request, Body, File, UploadFile, Form
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import pickle
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 with open( 'rfc_model_pickle', 'rb' ) as file:
     rf_pickle = pickle.load( file )
 
-templates = Jinja2Templates( directory="templates" )
-
 app = FastAPI()
+
 
 
 class Item( BaseModel ):
@@ -20,39 +17,27 @@ class Item( BaseModel ):
 
 
 @app.get( '/' )
-async def home(request: Request):
-    return templates.TemplateResponse( "index.html", {"request": request} )
+def home():
+    return {'message': 'Welcome to Hotspots analyzer'}
 
 
-@app.post( '/submitform' )
-async def handle_form(request: Request , startPosition: int = Form( ... ),
-                      mut_count: int = Form( ... ),
-                      total_mut_count: int = Form( ... ),
-                      ):
+@app.post( '/hotspots' )
+def hotspots_pred(data: Item):
+    data = data.dict()
+    startPosition = data['startPosition']
+    mut_count = data['mut_count']
+    total_mut_count = data['total_mut_count']
+
     prediction = rf_pickle.predict( [[startPosition, mut_count, total_mut_count]] )
     if prediction == 1:
         hotspot = "It's a cancer hotspot"
     else:
         hotspot = "It's not a cancer hotspot"
-    print(prediction)
-    print(hotspot)
-    return templates.TemplateResponse( "index_submit.html", {"request": request, "prediction":prediction, "hotspot":hotspot} )
 
-
-# @app.post( '/hotspots' )
-# async def hotspots_pred(data: Item, request: Request):
-#     data = data.dict()
-#     startPosition = data['startPosition']
-#     mut_count = data['mut_count']
-#     total_mut_count = data['total_mut_count']
-#
-#     prediction = rf_pickle.predict( [[startPosition, mut_count, total_mut_count]] )
-#     if prediction == 1:
-#         hotspot = "It's a cancer hotspot"
-#     else:
-#         hotspot = "It's not a cancer hotspot"
-#
-#     return templates.TemplateResponse("index_2.html", {"request": request, "prediction": prediction, "hotspot": hotspot})
+    return {
+        "Prediction":int(prediction),
+        "Cancer Hotspot Prediction": hotspot
+    }
 
 
 if __name__ == "__main__":
